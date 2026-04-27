29a4ef2be2a9e343ced48bf7bbe1dc2c83526b5d
ratihsal <bilasalsaratih@gmail.com>
2026-04-27 18:37:56 +0700

    database

--- backend/src/config/supabase.js
+++ backend/src/config/supabase.js
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-const supabaseUrl = "https://jbtygeofamxmgthhvkwv.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpidHlnZW9mYW14bWd0aGh2a3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNjcxMTcsImV4cCI6MjA5Mjg0MzExN30.rW_OjaO21qMAYc0YJFssT4UgXdmTUBarbfQiiIRVZCI";
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
